08a2fdfb25cba87076467e15318a670208e82c42
Alejandro-Perezg <a01746643@tec.mx>
2023-03-18 00:45:38 -0600

    All functions implemented, waiting for minimax

--- Primer-periodo/Proyect 1/tictactoe/tictactoe.py
+++ Primer-periodo/Proyect 1/tictactoe/tictactoe.py
@@ -1,5 +1,8 @@
 """
 Tic Tac Toe Player
+Authors: 
+Alejandro Perez Gonzalez A01746643
+
 """
 
 import math
@@ -27,10 +30,9 @@ def player(board):
     
 
     for row in board:
-        if row == X:
-            NumberOfX =+ 1
-        if  row == O:
-            NumberOfO =+ 1
+        NumberOfX += row.count(X)
+        NumberOfO += row.count(O)
+
 
     if NumberOfX > NumberOfO:
         return O
@@ -44,14 +46,14 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    plays = []
+    playerMoves = []
     
     for i in range(3):
         for j in range(3):
-            if board[i][j] == None:
-                plays.append(i,j)
+            if board[i][j] == EMPTY:
+                playerMoves.append((i,j))
     
-    return plays
+    return playerMoves
 
    # raise NotImplementedError
 
@@ -59,33 +61,81 @@ def actions(board):
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
-    """
-    raise NotImplementedError
+    """    
+    Board = [row[:] for row in board]
+
+    if Board[action[0]][action[1]] != EMPTY:
+        raise ValueError("Position already occupied")
+
+    
+    
+    board_copy = deepcopy(board)
+    board_copy[i][j] = player(board)
+    return board_copy
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    #Diagonal
+    if board[0][0] and board[1][1] and board[2][2] == X:
+        return X
+    if board[0][0] and board[1][1] and board[2][2] == O:
+        return O
+
+    #Rows
+    for row in board:
+        if row.count(X) == 3:
+            return X
+        if row.count(O) == 3:
+            return O
+    #Columns
+    cols = ''
+    for i in range(3):
+        for j in range(3):
+            cols += str(board[i][j])
+    if cols == 'XXX':
+        return X
+    if cols == 'OOO':
+        return O
+
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    # Check winner function to determine if there is a winner
+    if winner(board):
+        return True
+
+    # Check if the board is empty
+    for row in board:
+        if EMPTY in row:
+            return False
+        else:
+            return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
+
+
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
+    'X' Player is trying to maximise the score, 'O' Player is trying to minimise it
     """
     raise NotImplementedError
+
